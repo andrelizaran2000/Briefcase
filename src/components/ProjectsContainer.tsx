c2fa2bd578a21d1ca9a5d3ea0e53783e72f84d7a
andrelizaran <andrelizaran2000@gmail.com>
2022-06-29 19:27:33 -0500

    feat: Estilos de proyectos

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -1,5 +1,7 @@
 // Modules
 import { Box } from '@mui/system'
+import { useTranslation } from 'react-i18next';
+import { grey, lime } from '@mui/material/colors';
 import { Grid, Paper, Stack, Typography } from '@mui/material'
 
 // Components
@@ -7,8 +9,7 @@ import { GrayTitle } from './Common'
 
 // Data
 import { useNavigate } from 'react-router-dom';
-import { lime } from '@mui/material/colors';
-import { useTranslation } from 'react-i18next';
+
 
 export default function ProjectsContainer() {
 
@@ -24,19 +25,22 @@ export default function ProjectsContainer() {
         {projects.map(({ title, description, technologies, id }) => (
          <Grid item md={6} lg={4} key={title} sx={{ width:'100%' }}>
             <Stack 
-              sx={{ border:2, borderColor:'white', borderRadius:2, padding:2, color:'white', alignItems:'center', textAlign:'center', justifyContent:'center' }}
-              style={{ minHeight:140, cursor:'pointer' }} 
-              rowGap={2}
+              sx={{ border:2, borderColor:'white', borderRadius:2, color:'white', alignItems:'center', textAlign:'center', justifyContent:'center' }}
+              style={{ cursor:'pointer' }} 
               onClick={() => navigate(`/project/${id}`)}
             >
-              <Typography variant='h6' component='h4'>{title}</Typography>
-              <Typography variant='body2'>{description}</Typography>
-              <Stack direction='row' gap={1} flexWrap='wrap' justifyContent='center'>
-                {technologies.map((technology:any) => (
-                <Paper sx={{ padding:1, backgroundColor:lime[700] }} key={technology}>
-                  <Typography variant='caption' sx={{ color:'white' }}>{technology}</Typography>
-                </Paper>
-                ))}
+              <Box sx={{ backgroundColor:'white', display:'flex', alignItems:'center', justifyContent:'center', width:'100%', paddingY:2 }}>
+                <Typography variant='h6' component='h4' color={grey[900]}>{title}</Typography>
+              </Box>
+              <Stack sx={{ padding:2, rowGap:2 }}>
+                <Typography variant='body2'>{description}</Typography>
+                <Stack direction='row' gap={1} flexWrap='wrap' justifyContent='center'>
+                  {technologies.map((technology:any) => (
+                  <Paper sx={{ padding:1, backgroundColor:lime[700] }} key={technology}>
+                    <Typography variant='caption' sx={{ color:'white' }}>{technology}</Typography>
+                  </Paper>
+                  ))}
+                </Stack>
               </Stack>
             </Stack>
          </Grid>
